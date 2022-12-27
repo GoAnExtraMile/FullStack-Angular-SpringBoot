efa3fdf000988ffb28ec0ac5ce9c3ba8da11e411
CuriousDeveloperMom <prit.solanki@gmail.com>
2022-12-27 03:25:26 -0500

    Some minor changes, Created Home Component

--- frontend/empsystem/src/app/app.component.ts
+++ frontend/empsystem/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "empsystem";
+  title = "Employee Management System";
+  message = "Welcome to ";
 }
