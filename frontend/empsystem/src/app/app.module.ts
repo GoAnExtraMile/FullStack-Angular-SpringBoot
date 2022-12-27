9ee3cefd303d3df4d29c09423d7f9bd02a31defa
CuriousDeveloperMom <prit.solanki@gmail.com>
2022-12-27 14:42:41 -0500

    Created ListEmployees Component

--- frontend/empsystem/src/app/app.module.ts
+++ frontend/empsystem/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ListemployeesComponent } from './listemployees/listemployees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ListemployeesComponent
   ],
   imports: [
     BrowserModule,
