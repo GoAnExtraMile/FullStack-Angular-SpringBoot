c115f9d237898d2f65c81df54a867795c1cbefb4
CuriousDeveloperMom <prit.solanki@gmail.com>
2022-12-27 23:57:10 -0500

    Created Array of Employees to hold List of employees in listemployees.component.ts, Updated listemployees.component.html to show list in the form of table"

--- frontend/empsystem/src/app/listemployees/listemployees.component.ts
+++ frontend/empsystem/src/app/listemployees/listemployees.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Employee } from "../employee";
 @Component({
-  selector: 'app-listemployees',
-  templateUrl: './listemployees.component.html',
-  styleUrls: ['./listemployees.component.css']
+  selector: "app-listemployees",
+  templateUrl: "./listemployees.component.html",
+  styleUrls: ["./listemployees.component.css"],
 })
 export class ListemployeesComponent implements OnInit {
+  employees: Employee[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.employees = [
+      {
+        id: 1,
+        firstName: "Priti",
+        lastName: "Solanki",
+        emailId: "ps@xyz.com",
+      },
+      {
+        id: 2,
+        firstName: "Abhi",
+        lastName: "Solanki",
+        emailId: "as@xyz.com",
+      },
+      {
+        id: 3,
+        firstName: "Aarya",
+        lastName: "Solanki",
+        emailId: "aas@xyz.com",
+      },
+    ];
   }
-
 }
