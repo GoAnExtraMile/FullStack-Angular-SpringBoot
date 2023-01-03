67471ecbc078bfeace999132beed8b984327ce40
CuriousDeveloperMom <prit.solanki@gmail.com>
2023-01-03 14:25:25 -0500

    Configure routing to for creat-employee, updated app component header section with the link of AddEmployee

--- frontend/empsystem/src/app/app-routing.module.ts
+++ frontend/empsystem/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ListemployeesComponent } from "./listemployees/listemployees.component";
+import { CreateEmployeeComponent } from "./create-employee/create-employee.component";
 
 const routes: Routes = [
   { path: "employees", component: ListemployeesComponent },
+  { path: "create-employee", component: CreateEmployeeComponent },
   { path: "", redirectTo: "employees", pathMatch: "full" },
 ];
 
