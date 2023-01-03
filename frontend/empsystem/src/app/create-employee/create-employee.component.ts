6f4a9643868b31cd37bb3616a5a128a9e9b15f12
CuriousDeveloperMom <prit.solanki@gmail.com>
2023-01-03 14:43:29 -0500

    define employee property in create-employee component of type Employee.ts

--- frontend/empsystem/src/app/create-employee/create-employee.component.ts
+++ frontend/empsystem/src/app/create-employee/create-employee.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Employee } from "../employee";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"],
 })
 export class CreateEmployeeComponent implements OnInit {
+  employee: Employee = new Employee();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
